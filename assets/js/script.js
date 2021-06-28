04cb165a2e7f1a524df825318c2bb2ef986a5e89
Anthony Pena <apena5@alumni.jh.edu>
2021-06-28 12:40:47 -0600

    added Maria Sanchez assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,8 @@ var x = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days + "D " + hours + "H "
+        + minutes + "M " + seconds + "S";
 
     // If the count down is over, write some text 
     if (distance < 0) {
