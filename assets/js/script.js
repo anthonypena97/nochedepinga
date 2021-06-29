6ea154512abc14421053fb0352e1e587d2d224fd
Anthony Pena <apena5@alumni.jh.edu>
2021-06-28 22:36:43 -0600

    countdown load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-$.ajaxSetup
-    ({
-        // Disable or Enable caching of AJAX
-        cache: false // or true
-    });
-
 var video = document.getElementById("myVideo");
 
 function myFunction() {
@@ -21,32 +15,35 @@ function fadeIn() {
 
 setTimeout(fadeIn, 1000);
 
-// Set the date we're counting down to
-// Set to Eastern Standard Time
-var countDownDate = new Date("2021-07-08T16:00:00.1-04:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
+$(document).ready(function () {
+    // Set the date we're counting down to
+    // Set to Eastern Standard Time
+    var countDownDate = new Date("2021-07-08T16:00:00.1-04:00").getTime();
 
-    // Get today's date and time
-    var now = new Date().getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function () {
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
 
-    // Output the result in an element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "TICKET LINK";
-    }
-}, 1000);
+        // Output the result in an element with id="demo"
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "TICKET LINK";
+        }
+    }, 1000);
+});
